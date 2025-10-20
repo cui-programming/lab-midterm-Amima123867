4b2b5eb7e1fc09b89c6b0632d4c6545d3e5aaf3e
Amima123867 <Sohailaqamar1@gmail.com>
2025-10-20 13:10:52 +0500

    Update TextInput.js

--- src/components/ui/TextInput.js
+++ src/components/ui/TextInput.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { TextInput as RNTextInput } from 'react-native';
 
+/**
+ * UI/TextInput
+ * Simple wrapper over React Native's TextInput
+ * Students can style this through styles.js or props
+ */
 export default function TextInput(props) {
   return <RNTextInput {...props} />;
 }
